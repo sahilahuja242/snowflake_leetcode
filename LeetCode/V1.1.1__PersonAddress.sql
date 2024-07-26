bf605430678b479b0eade9b9cff3ddfd3a3d11f3
sahil ahuja <sahilahuja242@gmail.com>
2024-07-26 12:45:55 +0530

    add

--- LeetCode/V1.1.1__PersonAddress.sql
+++ LeetCode/V1.1.1__PersonAddress.sql
@@ -7,3 +7,9 @@ Truncate table LEETCODE.LEETCODE.Address;
 insert into LEETCODE.LEETCODE.Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York');
 insert into LEETCODE.LEETCODE.Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California');
 
+-- Question
+-- Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+-- If the address of a personId is not present in the Address table, report null instead.
+--Solution
+-- select p.firstname, p.lastname, a.city,a.state from PERSON p  left join ADDRESS a on a.personid=p.personid;
+
