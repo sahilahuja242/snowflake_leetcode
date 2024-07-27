1d3bdcdd0cb2fb9ca4573969a045b0ee42c5d25d
sahil ahuja <sahilahuja242@gmail.com>
2024-07-27 10:50:44 +0530

    add

---
+++ LeetCode/V1.1.2__Employee.sql
@@ -0,0 +1,6 @@
+Create table If Not Exists LEETCODE.LEETCODE.Employee (id int, name varchar(255), salary int, managerId int);
+Truncate table LEETCODE.LEETCODE.Employee;
+insert into LEETCODE.LEETCODE.Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
+insert into LEETCODE.LEETCODE.Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
+insert into LEETCODE.LEETCODE.Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None');
+insert into LEETCODE.LEETCODE.Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None');
