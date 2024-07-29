6bd801ea0e924b02067386c965c716323e979de9
sahil ahuja <sahilahuja242@gmail.com>
2024-07-29 14:38:32 +0530

    add

--- LeetCode/V1.1.3__PersonEmail.sql
+++ LeetCode/V1.1.3__PersonEmail.sql
@@ -1,5 +1,5 @@
-Create table If Not Exists Person (id int, email varchar(255));
-Truncate table Person;
-insert into Person (id, email) values ('1', 'a@b.com');
-insert into Person (id, email) values ('2', 'c@d.com');
-insert into Person (id, email) values ('3', 'a@b.com');
+Create table If Not Exists PersonEmail (id int, email varchar(255));
+Truncate table PersonEmail;
+insert into PersonEmail (id, email) values ('1', 'a@b.com');
+insert into PersonEmail (id, email) values ('2', 'c@d.com');
+insert into PersonEmail (id, email) values ('3', 'a@b.com');
