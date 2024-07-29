7346c23c10b5ad1add664403ce194af0b1e6298e
sahil ahuja <sahilahuja242@gmail.com>
2024-07-29 14:43:32 +0530

    add

--- LeetCode/V1.1.3__PersonEmail.sql
+++ LeetCode/V1.1.3__PersonEmail.sql
@@ -3,3 +3,8 @@ Truncate table PersonEmail;
 insert into PersonEmail (id, email) values ('1', 'a@b.com');
 insert into PersonEmail (id, email) values ('2', 'c@d.com');
 insert into PersonEmail (id, email) values ('3', 'a@b.com');
+
+--Quest
+--Write a solution to report all the duplicate emails. 
+--Solution
+--select email from PERSONEMAIL group by email having count(email)>1;
